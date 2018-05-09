7a479cf0ed098f422ca10ae5f2966cb1b55d6668
karverx <karverx@gmail.com>
2018-05-09 13:13:00 +0300

    first commit

---
+++ src/main/java/ru/levelp/myproject/model/User.java
@@ -0,0 +1,17 @@
+package ru.levelp.myproject.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column
+    private String Discription;
+}
